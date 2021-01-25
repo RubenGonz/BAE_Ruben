a22a77540f7a077d548dde2c5e0421f53d778aed
RubenGonz <ruben30303030@gmail.com>
2021-01-25 19:33:08 +0000

    Avance de las practicas

--- ud4/practicas/Practica4_2_ConsultasEnSqlEmpleados.sql
+++ ud4/practicas/Practica4_2_ConsultasEnSqlEmpleados.sql
@@ -77,7 +77,7 @@ SELECT * FROM emple;
 
 --4 Datos de los empleados ordenados por apellidos.
 
-SELECT * FROM emple ORDER BY apellidos ASC;
+SELECT * FROM emple ORDER BY apellido ASC;
 
 --5 Datos de los empleados ordenados por número de departamento descendentemente.
 
@@ -85,31 +85,39 @@ SELECT * FROM emple ORDER BY dept_no DESC;
 
 --6 Datos de los empleados ordenados por número de departamento descendentemente y dentro de cada departamento ordenados por apellido ascendentemente.
 
-
+SELECT * FROM emple ORDER BY dept_no DESC, apellido ASC;
 
 --8 Mostrar los datos de los empleados cuyo salario sea mayor que 2000000. 
 
+SELECT * FROM emple WHERE salario>2000000;
 
 --9 Mostrar los datos de los empleados cuyo oficio sea ʻANALISTAʼ.
 
+SELECT * FROM emple WHERE oficio='ANALISTA';
    
 --10 Seleccionar el apellido y oficio de los empleados del departamento número 20.
 
+SELECT apellido, oficio FROM emple WHERE dept_no='20';
 
 --11 Mostrar todos los datos de los empleados ordenados por apellido.
 
+SELECT * FROM emple ORDER BY apellidos ASC;
 
 --12 Seleccionar los empleados cuyo oficio sea ʻVENDEDORʼ. Mostrar los datos ordenados por apellido.
 
+SELECT * FROM emple WHERE oficio='VENDEDOR' ORDER BY apellido ASC;
 
 --13 Mostrar los empleados cuyo departamento sea 10 y cuyo oficio sea ʻANALISTAʼ. Ordenar el resultado por apellido.
 
+SELECT * FROM emple WHERE dept_no='10'AND oficio='ANALISTA' ORDER BY apellido ASC;
 
 --14 Mostrar los empleados que tengan un salario mayor que 200000 o que pertenezcan al departamento número 20.
 
+SELECT * FROM emple WHERE salario>200000 OR dept_no='20';
 
 --15 Ordenar los empleados por oficio, y dentro de oficio por nombre.
 
+SELECT * FROM emple ORDER BY oficio ASC, emp_no ASC;
 
 --16 Seleccionar de la tabla EMPLE los empleados cuyo apellido empiece por ʻAʼ.
 
@@ -121,29 +129,38 @@ SELECT * FROM emple WHERE apellido LIKE '%Z';
 
 --18 Seleccionar de la tabla EMPLE aquellas filas cuyo APELLIDO empiece por ʻAʼ y el OFICIO tenga una ʻEʼ en cualquier posición.
 
+SELECT * FROM emple WHERE apellido LIKE 'A%' AND 'E';
 
 --19 Seleccionar los empleados cuyo salario esté entre 100000 y 200000. Utilizar el operador BETWEEN.
 
+SELECT * FROM emple WHERE salario BETWEEN '100000' AND '200000';
 
 --20 Obtener los empleados cuyo oficio sea ʻVENDEDORʼ y tengan una comisión superior a 100000.
 
+SELECT * FROM emple WHERE oficio='VENDEDOR'AND comision>'100000';
 
 --21 Seleccionar los datos de los empleados ordenados por número de departamento, y dentro de cada departamento ordenados por apellido.
 
+SELECT * FROM emple ORDER BY dept_no ASC, apellido ASC;
 
 --22 Número y apellidos de los empleados cuyo apellido termine por ʻZʼ y tengan un salario superior a 300000.
 
+SELECT emp_no, apellido FROM emple WHERE apellido LIKE '%Z' AND salario>'300000';
 
 --23. Datos de los departamentos cuya localización empiece por ʻBʼ.
 
+SELECT * FROM departamentos WHERE loc LIKE 'B%';
 
 --24. Datos de los empleados cuyo oficio sea ʻEMPLEADOʼ, tengan un salario superior a 100000 y pertenezcan al departamento número 10.
 
+SELECT * FROM emple WHERE oficio='EMPLEADO' AND salario>'100000' AND dept_no='10';
 
 --25. Mostrar los apellidos de los empleados que no tengan comisión.
 
+SELECT apellido FROM emple WHERE comision IS 'NULL';
 
 --26. Mostrar los apellidos de los empleados que no tengan comisión y cuyo apellido empiece por ʻJʼ.
+
 
 
 --27. Mostrar los apellidos de los empleados cuyo oficio sea ʻVENDEDORʼ, ʻANALISTAʼ o ʻEMPLEADOʼ.
